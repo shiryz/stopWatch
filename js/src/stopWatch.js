2480f1f9f37acfb01f37436cf839271d47bcafd7
Peter Rhodes <rhodepeter@hotmail.co.uk>
2016-10-26 18:01:23 +0100

    Add correct functionality to js file.

--- js/src/stopWatch.js
+++ js/src/stopWatch.js
@@ -1,21 +1,30 @@
-var d = new Date();
+var d ;
 var startTime = 0;
 var stopTime = 0;
+var flag;
 
 function start(){
+  clearInterval(flag);
+  document.getElementById('watch').innerHTML = '00:00:00:00';
   console.log('START')
-
   d = new Date();
   startTime= d.getTime();
+  flag = setInterval(function(){
+    d = new Date();
+    stopTime = d.getTime();
+    var rightFormat = msToTime(stopTime - startTime);
+    document.getElementById('watch').innerHTML = rightFormat;
+  },1);
 }
 
 function stop(){
+  clearInterval(flag);
   console.log('STOP')
   d = new Date();
   stopTime = d.getTime();
   //console.log(time);
-  var rightFormat = msToTime(stopTime - startTime);
-  document.getElementById('watch').innerHTML = rightFormat;
+  //var rightFormat = msToTime(stopTime - startTime);
+  //document.getElementById('watch').innerHTML = rightFormat;
 }
 
 function reset(){
@@ -24,16 +33,18 @@ function reset(){
 
 function msToTime(duration) {
       var x;
+      //x=Math.floor(duration%1000);
+      var milli = ((duration % 1000) / 10).toFixed(0);
       x=Math.floor(duration/1000);
-      console.log(x);
       var seconds = x % 60;
-      console.log(seconds);
       x=Math.floor(x/60);
-      console.log(x);
       var minutes = x % 60;
       x=Math.floor(x/60);
-      console.log(x);
       var hours= x%24;
+      if(hours < 10) hours="0" + hours;
+      if(minutes < 10) minutes="0" + minutes;
+      if(seconds < 10) seconds="0" + seconds;
+      if(milli < 10) milli="0" + milli;
 
-      return hours + ":" + minutes + ":" + seconds ;
+      return hours + ":" + minutes + ":" + seconds + ":" + milli ;
   }
