79da12706af8ab49df902d8563c3a2a39790fb94
Peter Rhodes <rhodepeter@hotmail.co.uk>
2016-10-26 16:52:53 +0100

    Add counter function

--- js/src/stopWatch.js
+++ js/src/stopWatch.js
@@ -1,18 +1,39 @@
-var d;
-var time = 0;
+var d = new Date();
+var startTime = 0;
+var stopTime = 0;
 
 function start(){
   console.log('START')
-  console.log(0)
+
   d = new Date();
+  startTime= d.getTime();
 }
 
 function stop(){
   console.log('STOP')
-  time = d.getMilliseconds();
-  console.log(time);
+  d = new Date();
+  stopTime = d.getTime();
+  //console.log(time);
+  var rightFormat = msToTime(stopTime - startTime);
+  document.getElementById('watch').innerHTML = rightFormat;
 }
 
 function reset(){
   console.log('RESET')
 }
+
+function msToTime(duration) {
+      var x;
+      x=Math.floor(duration/1000);
+      console.log(x);
+      var seconds = x % 60;
+      console.log(seconds);
+      x=Math.floor(x/60);
+      console.log(x);
+      var minutes = x % 60;
+      x=Math.floor(x/60);
+      console.log(x);
+      var hours= x%24;
+
+      return hours + ":" + minutes + ":" + seconds ;
+  }
